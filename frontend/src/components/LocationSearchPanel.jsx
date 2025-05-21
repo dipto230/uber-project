a9c2480db7133034091da784aa6fddd0c9167858
dipto230 <diptoroy230305@gmail.com>
2025-05-21 14:40:13 +0600

    location search panel done with sample data frontend

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+      <div>
+          {/* this is just sample data*/}
+          <div className='flex gap-4 items-center my-5 justify-center'>
+              <h2 className='bg-[#eee] h-10 w-10 rounded-full flex items-center justify-center '><i className="ri-map-pin-fill"></i></h2>
+              <h4 className='font-medium'>24B, Near Kapoor's cafe,Sheryians Coding School, Bhopal</h4>
+          </div>
+             <div className='flex gap-4 items-center my-5 justify-center'>
+              <h2 className='bg-[#eee] h-10 w-10 rounded-full flex items-center justify-center '><i className="ri-map-pin-fill"></i></h2>
+              <h4 className='font-medium'>24B, Near Kapoor's cafe,Sheryians Coding School, Bhopal</h4>
+          </div>
+             <div className='flex gap-4 items-center my-5 justify-center'>
+              <h2 className='bg-[#eee] h-10 w-10 rounded-full flex items-center justify-center '><i className="ri-map-pin-fill"></i></h2>
+              <h4 className='font-medium'>24B, Near Kapoor's cafe,Sheryians Coding School, Bhopal</h4>
+          </div>
+    </div>
+  )
+}
+
+export default LocationSearchPanel
